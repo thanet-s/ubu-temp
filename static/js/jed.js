ce91131d63a01e16c98ccbf2197322b609b3c1e3
thanet-s <cho.112543@gmail.com>
2021-03-15 20:10:01 +0700

    remove jed

--- static/js/jed.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('input[type=number]')
-    .forEach(e => e.oninput = () => {
-        // Always 2 digits
-        if (e.value.length >= 2) e.value = e.value.slice(0, 2);
-        // 0 on the left (doesn't work on FF)
-        if (e.value.length === 1) e.value = '0' + e.value;
-        // Avoiding letters on FF
-        if (!e.value) e.value = '00';
-    });
